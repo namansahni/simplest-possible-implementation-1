6c947e4d4792d92fa7ae2f584e3495a09833fb54
namansahni <naman.sahni10@gmail.com>
2021-01-04 12:48:59 +0530

    Simplest Possible Implementation

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,14 @@
+package simplest.possible.implementation;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PrimeFactors {
+
+    private int number;
+
+    public List<Object> of(int number) {
+        this.number = number;
+        return Collections.emptyList();
+    }
+}
